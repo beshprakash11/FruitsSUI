443de6b2738fb9ef46c54a2debab2c877efd9ec8
beshprakash11 <beshprakashktm@gmail.com>
2023-02-24 14:07:49 +0100

    Project templates

---
+++ FruitsSUI/FruitsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsSUIApp.swift
+//  FruitsSUI
+//
+//  Created by Besh P.Yogi on 24.02.23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
