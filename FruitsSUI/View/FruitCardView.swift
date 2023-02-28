1ff7a0cf017ec3e8f3a901b48a076c70ddbc8cda
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 12:34:06 +0100

    berry image added

--- FruitsSUI/View/FruitCardView.swift
+++ FruitsSUI/View/FruitCardView.swift
@@ -13,6 +13,11 @@ struct FruitCardView: View {
     var body: some View {
         VStack(spacing: 20){
             //FRUIT: IMAGE
+            Image("blueberry")
+                .resizable()
+                .scaledToFit()
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 0, x: 6, y: 0)
+            
             
             //FRUIT: TITLE
             Text("Blueberry")
