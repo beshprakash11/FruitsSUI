c62a49cdc2b366a8d217a0893dd4a652949d45db
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 12:48:29 +0100

    documentation updated

--- FruitsSUI/View/StartButton.swift
+++ FruitsSUI/View/StartButton.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct StartButton: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+//MARK: - PREVIEW
 struct StartButton_Previews: PreviewProvider {
     static var previews: some View {
         StartButton()
