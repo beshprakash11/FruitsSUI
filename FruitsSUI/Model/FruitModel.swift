5979c70b895a2c641f8f48b3134c4b1ee782f2de
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 13:17:16 +0100

    fruitdata model updated

--- FruitsSUI/Model/FruitModel.swift
+++ FruitsSUI/Model/FruitModel.swift
@@ -9,4 +9,13 @@ import SwiftUI
 
 //MARK:- FRUIT DATA MODEL
 
-
+struct Fruit: Identifiable{
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+    
+}
