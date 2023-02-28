584906ce5a75656ee9ea504a28300e42026f82db
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 12:27:03 +0100

    FruitCardView added

---
+++ FruitsSUI/View/FruitCardView.swift
@@ -0,0 +1,22 @@
+//
+//  FruitCardView.swift
+//  FruitsSUI
+//
+//  Created by Besh P.Yogi on 28.02.23.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    // MARK: - PROPERTIES
+    //MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+    }
+}
