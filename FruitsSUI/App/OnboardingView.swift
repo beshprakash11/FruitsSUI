c1cb2018a1a6cf54d2ebd35aa25bde5beed6d2ee
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 13:07:35 +0100

    onboarding view updated

---
+++ FruitsSUI/App/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  FruitsSUI
+//
+//  Created by Besh P.Yogi on 28.02.23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
