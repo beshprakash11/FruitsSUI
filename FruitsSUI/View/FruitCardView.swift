763fe148a8d7150b23c7e3469a00f842b9a6c8ab
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 12:44:19 +0100

    title styling changes

--- FruitsSUI/View/FruitCardView.swift
+++ FruitsSUI/View/FruitCardView.swift
@@ -22,6 +22,10 @@ struct FruitCardView: View {
                 
                 //FRUIT: TITLE
                 Text("Blueberry")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
                 
                 //FRUIT: HEADLINE
                 
