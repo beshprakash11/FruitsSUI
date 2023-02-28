1ef49ded9195e536e8a0b2ca599153e54c1534bf
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 12:37:22 +0100

    layout updated

--- FruitsSUI/View/FruitCardView.swift
+++ FruitsSUI/View/FruitCardView.swift
@@ -11,21 +11,23 @@ struct FruitCardView: View {
     // MARK: - PROPERTIES
     //MARK: - BODY
     var body: some View {
-        VStack(spacing: 20){
-            //FRUIT: IMAGE
-            Image("blueberry")
-                .resizable()
-                .scaledToFit()
-                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 0, x: 6, y: 0)
-            
-            
-            //FRUIT: TITLE
-            Text("Blueberry")
-            
-            //FRUIT: HEADLINE
-            
-            //BUTTON: START
-        }//: VStack
+        ZStack{
+            VStack(spacing: 20){
+                //FRUIT: IMAGE
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 0, x: 6, y: 0)
+                
+                
+                //FRUIT: TITLE
+                Text("Blueberry")
+                
+                //FRUIT: HEADLINE
+                
+                //BUTTON: START
+            }//: VStack
+        }//:ZStack
     }
 }
 
