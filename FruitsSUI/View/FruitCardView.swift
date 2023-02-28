0cdfee0256564e0f2a276d01fbb70d0a672889fc
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 13:54:10 +0100

    dynamic gradient colors

--- FruitsSUI/View/FruitCardView.swift
+++ FruitsSUI/View/FruitCardView.swift
@@ -49,7 +49,7 @@ struct FruitCardView: View {
             }
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .top, endPoint: .bottom))
+        .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
         .padding(.horizontal, 20)
     }
