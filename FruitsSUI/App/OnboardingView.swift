eb300578abe0e44151af90c5ab13efaeff1cd190
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 13:08:44 +0100

    init documentation

--- FruitsSUI/App/OnboardingView.swift
+++ FruitsSUI/App/OnboardingView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+
+//MARK:  PREVIEW
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
         OnboardingView()
