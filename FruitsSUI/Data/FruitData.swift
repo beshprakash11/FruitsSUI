6414400cdc23baf643664665441b6829b7b5849d
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 13:31:34 +0100

    berries data updated

--- FruitsSUI/Data/FruitData.swift
+++ FruitsSUI/Data/FruitData.swift
@@ -8,6 +8,34 @@
 import SwiftUI
 //MARK: - FRUITS DATA
 let fruitsData: [Fruit] = [
-    Fruit(title: "Bluberry", headline: "Bluberries are swaet, nutritious and widly popular fruits all over the workd.", image: "blueberry", gradientColors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")], description: "Blueberries are parennial flowering plangs with blue or purple berries.", nutrition: ["240 kJ", "9.96 g", "0.33 g", "A, B1", "Calcium, Magnasium"]),
-    
+    Fruit(
+        title: "Blueberry",
+        headline: "Blueberries are sweet, nutritious and wildly popular fruit all over the world.",
+        image: "blueberry",
+        gradientColors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")],
+        description: """
+          Blueberries are perennial flowering plants with blue or purple berries. They are classified in the section Cyanococcus within the genus Vaccinium. Vaccinium also includes cranberries, bilberries, huckleberries and Madeira blueberries.
+
+          Commercial blueberries—both wild (lowbush) and cultivated (highbush)—are all native to North America. The highbush varieties were introduced into Europe during the 1930s.
+
+          Blueberries are usually prostrate shrubs that can vary in size from 10 centimeters (3.9 in) to 4 meters (13 ft) in height. In commercial production of blueberries, the species with small, pea-size berries growing on low-level bushes are known as "lowbush blueberries" (synonymous with "wild"), while the species with larger berries growing on taller cultivated bushes are known as "highbush blueberries".
+
+          Canada is the leading producer of lowbush blueberries, while the United States produces some 40% of the world supply of highbush blueberries.
+
+          USES
+
+          Blueberries are sold fresh or are processed as individually quick frozen (IQF) fruit, purée, juice, or dried or infused berries. These may then be used in a variety of consumer goods, such as jellies, jams, blueberry pies, muffins, snack foods, or as an additive to breakfast cereals.
+
+          Blueberry jam is made from blueberries, sugar, water, and fruit pectin. Blueberry sauce is a sweet sauce prepared using blueberries as a primary ingredient.
+
+          Blueberry wine is made from the flesh and skin of the berry, which is fermented and then matured; usually the lowbush variety is used.
+
+          NUTRIENTS
+
+          Blueberries consist of 14% carbohydrates, 0.7% protein, 0.3% fat and 84% water (table). They contain only negligible amounts of micronutrients, with moderate levels (relative to respective Daily Values) (DV) of the essential dietary mineral manganese, vitamin C, vitamin K and dietary fiber (table).
+
+          Generally, nutrient contents of blueberries are a low percentage of the DV (table). One serving provides a relatively low caloric value of 57 kcal with a glycemic load of 6.
+          """,
+        nutrition: ["240 kJ (57 kcal)","9.96 g","0.33 g","0.74 g","A, B1, B2, B3, B5, B6, B9, C, E, K","Calcium, Iron, Magnasium, Manganese, Phosphorus, Sodium, Zinc"]
+      ),
 ]
