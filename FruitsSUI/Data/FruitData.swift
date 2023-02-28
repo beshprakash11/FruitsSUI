2350abc75d3a1c33ba93994c84f7e76bf8c256c9
beshprakash11 <beshprakashktm@gmail.com>
2023-02-28 13:25:43 +0100

    berreis added in fruits data

--- FruitsSUI/Data/FruitData.swift
+++ FruitsSUI/Data/FruitData.swift
@@ -5,4 +5,9 @@
 //  Created by Besh P.Yogi on 28.02.23.
 //
 
-import Foundation
+import SwiftUI
+//MARK: - FRUITS DATA
+let fruitsData: [Fruit] = [
+    Fruit(title: "Bluberry", headline: "Bluberries are swaet, nutritious and widly popular fruits all over the workd.", image: "blueberry", gradientColors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")], description: "Blueberries are parennial flowering plangs with blue or purple berries.", nutrition: ["240 kJ", "9.96 g", "0.33 g", "A, B1", "Calcium, Magnasium"]),
+    
+]
