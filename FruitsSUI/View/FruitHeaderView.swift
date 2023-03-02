9f728443c22e3abdb70c09c6890fc4339bd7bdbc
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 13:37:52 +0100

    animate updated

--- FruitsSUI/View/FruitHeaderView.swift
+++ FruitsSUI/View/FruitHeaderView.swift
@@ -23,6 +23,7 @@ struct FruitHeaderView: View {
                 .scaledToFit()
                 .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15),radius: 8, x: 6, y: 8)
                 .padding(.vertical, 20)
+                .scaleEffect(isAnimatingImage ? 1.0 : 0.6)
         }//:ZStack
         .frame(height: 440)
         .onAppear(){
