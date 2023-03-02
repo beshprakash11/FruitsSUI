4cfada20eedf38bdb560596bedb51105245fc699
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 12:41:51 +0100

    fruit card view updated

--- FruitsSUI/View/FruitCardView.swift
+++ FruitsSUI/View/FruitCardView.swift
@@ -16,29 +16,30 @@ struct FruitCardView: View {
     //MARK: - BODY
     var body: some View {
         ZStack{
-            VStack(spacing: 20){
-                //FRUIT: IMAGE
-                Image(fruit.image)
-                    .resizable()
-                    .scaledToFit()
-                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 0, x: 6, y: 0)
-                    .scaleEffect(isAnimating ? 1.0 : 0.6)
-                
-                
-                //FRUIT: TITLE
-                Text(fruit.title)
-                    .foregroundColor(.white)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
-                
-                //FRUIT: HEADLINE
-                Text(fruit.headline)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 16)
-                    .frame(maxWidth: 480)
-                
+            VStack(spacing: 10){
+                VStack(spacing: 20){
+                    //FRUIT: IMAGE
+                    Image(fruit.image)
+                        .resizable()
+                        .scaledToFit()
+                        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 0, x: 6, y: 0)
+                        .scaleEffect(isAnimating ? 1.0 : 0.6)
+                    
+                    
+                    //FRUIT: TITLE
+                    Text(fruit.title)
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                    
+                    //FRUIT: HEADLINE
+                    Text(fruit.headline)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 16)
+                        .frame(maxWidth: 480)
+                }
                 //BUTTON: START
                 StartButton()
             }//: VStack
