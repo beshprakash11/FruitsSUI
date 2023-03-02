ddae00ec5bacc6140ae36492950fc3b8463ef2d4
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 12:35:55 +0100

    screen switch variable added in app  storage

--- FruitsSUI/App/FruitsSUIApp.swift
+++ FruitsSUI/App/FruitsSUIApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct FruitsSUIApp: App {
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
     var body: some Scene {
         WindowGroup {
             OnboardingView()
