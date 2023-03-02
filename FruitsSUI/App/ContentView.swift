1cfb01cdeba2f567e45b571fed99952f6ce03c17
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 13:01:47 +0100

    display all fruit in content view updated

--- FruitsSUI/App/ContentView.swift
+++ FruitsSUI/App/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List{
-                ForEach(fruits) { item in
+                ForEach(fruits.shuffled()) { item in
                     FruitRowView(fruit: item)
                         .padding(.vertical, 4)
                 }
