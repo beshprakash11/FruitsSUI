5be0f0001c868e5fe2f90523cc880df0fbde7f54
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 13:51:51 +0100

    documentaton updated

--- FruitsSUI/View/FruitNutrientsView.swift
+++ FruitsSUI/View/FruitNutrientsView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct FruitNutrientsView: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
