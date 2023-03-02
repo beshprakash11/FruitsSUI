fffbb74066721e28b5f6ced5dd27c6a72027c9c0
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 13:24:13 +0100

    fruit header view

---
+++ FruitsSUI/View/FruitHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitHeaderView.swift
+//  FruitsSUI
+//
+//  Created by Besh P.Yogi on 02.03.23.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitHeaderView()
+    }
+}
