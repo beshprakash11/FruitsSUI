a8af61b2620b936b649c2bd12a6902488695bfe8
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 12:44:04 +0100

    row view documentation updated

--- FruitsSUI/View/RowView.swift
+++ FruitsSUI/View/RowView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct RowView: View {
+    //MARK: - PROPERTEIS
+    
+    
+    //MARK: - BODY
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
