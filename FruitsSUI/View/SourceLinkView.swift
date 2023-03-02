5b5105852f8475bc5abb44d63c54f3d86beae4c6
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 13:44:49 +0100

    content source added

--- FruitsSUI/View/SourceLinkView.swift
+++ FruitsSUI/View/SourceLinkView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct SourceLinkView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GroupBox(){
+            HStack{
+                Text("Content source")
+                Spacer()
+                Link("Wikipedia", destination: URL(string: "https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+            }//:hs
+            .font(.footnote)
+        }//:gb
     }
 }
 
