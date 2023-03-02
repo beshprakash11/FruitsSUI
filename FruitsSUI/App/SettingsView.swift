a1a64bd85755a4d8c38ec62822374742cb45095b
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 14:22:08 +0100

    setings view added

---
+++ FruitsSUI/App/SettingsView.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsView.swift
+//  FruitsSUI
+//
+//  Created by Besh P.Yogi on 02.03.23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    //MARK: - PROPERTIES
+    
+    
+    //MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
