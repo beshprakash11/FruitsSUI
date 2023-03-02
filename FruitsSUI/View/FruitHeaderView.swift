65f12013dff554476fa00f2283deb0fd55f23099
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 13:33:04 +0100

    header view updated

--- FruitsSUI/View/FruitHeaderView.swift
+++ FruitsSUI/View/FruitHeaderView.swift
@@ -9,17 +9,18 @@ import SwiftUI
 
 struct FruitHeaderView: View {
     //MARK: - PROPERTIES
-    
+    var fruit: Fruit
+        
     //MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(fruit.description)
     }
 }
 
 struct FruitHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        FruitHeaderView()
+        FruitHeaderView(fruit: fruitsData[0])
             .previewLayout(.fixed(width: 375, height: 440))
     }
 }
