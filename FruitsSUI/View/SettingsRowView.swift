2e492c96f51d6a67a49cea2cf78b2c4c3b490fd5
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 15:00:44 +0100

    setting row view addes

---
+++ FruitsSUI/View/SettingsRowView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsRowView.swift
+//  FruitsSUI
+//
+//  Created by Besh P.Yogi on 02.03.23.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsRowView()
+    }
+}
