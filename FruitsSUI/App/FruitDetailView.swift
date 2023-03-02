6eb71f2d87184ac12bbf2525148d5c3540a02c89
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 13:05:39 +0100

    detail view added

---
+++ FruitsSUI/App/FruitDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitDetailView.swift
+//  FruitsSUI
+//
+//  Created by Besh P.Yogi on 02.03.23.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitDetailView()
+    }
+}
