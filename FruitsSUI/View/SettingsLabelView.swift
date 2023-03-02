beacfe2069d962c3b2d0a676e6f23f36c3a0e8ee
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 14:38:31 +0100

    settings label view added

---
+++ FruitsSUI/View/SettingsLabelView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsLabelView.swift
+//  FruitsSUI
+//
+//  Created by Besh P.Yogi on 02.03.23.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView()
+    }
+}
