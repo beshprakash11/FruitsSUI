b254b3c8165aed540b1a057c17fe69fa575c0495
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 13:25:35 +0100

    documentation updated

--- FruitsSUI/View/FruitHeaderView.swift
+++ FruitsSUI/View/FruitHeaderView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct FruitHeaderView: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
