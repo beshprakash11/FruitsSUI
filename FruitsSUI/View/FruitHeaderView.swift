da826b93c2f04ae15cd7b2b93615b1bddfff24ff
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 13:35:59 +0100

    image added in header files

--- FruitsSUI/View/FruitHeaderView.swift
+++ FruitsSUI/View/FruitHeaderView.swift
@@ -16,7 +16,11 @@ struct FruitHeaderView: View {
     var body: some View {
         ZStack{
             LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .topLeading, endPoint: .bottomTrailing)
-            
+            Image(fruit.image)
+                .resizable()
+                .scaledToFit()
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15),radius: 8, x: 6, y: 8)
+                .padding(.vertical, 20)
         }//:ZStack
         .frame(height: 440)
     }
