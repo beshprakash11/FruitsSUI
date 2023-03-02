181987b1368e3d7c8de6216a377c14df2f50b4f2
beshprakash11 <beshprakashktm@gmail.com>
2023-03-02 13:51:02 +0100

    nutrients view added

---
+++ FruitsSUI/View/FruitNutrientsView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitNutrientsView.swift
+//  FruitsSUI
+//
+//  Created by Besh P.Yogi on 02.03.23.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitNutrientsView()
+    }
+}
