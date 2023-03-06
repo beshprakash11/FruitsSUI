23de1721d81f3c2728376c4440c05f52360647be
beshprakash11 <beshprakashktm@gmail.com>
2023-03-06 13:02:23 +0100

    project updated

--- FruitsSUI/App/SettingsView.swift
+++ FruitsSUI/App/SettingsView.swift
@@ -37,7 +37,7 @@ struct SettingsView: View {
                     
                     //MARK: - SECTION 3
                     GroupBox(
-                        label: SettingsLabelView(labelText: "Application", labelImage: "app.iphone")
+                        label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
                         
                     ){
                         Divider().padding(.vertical, 4)
